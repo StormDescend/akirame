224ebc93ab5de8293a78f7a5e1e468274c6e27c4
mayoi_Akira <akane36@163.com>
2025-12-25 15:47:31 +0800

    64 AC

--- cpp_solve/pta_64.cpp
+++ cpp_solve/pta_64.cpp
@@ -1,50 +1,37 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main()
-{
-    int t;
-    cin >> t;
-    getchar();
-    while (t--)
-    {
-        string s;
-        getline(cin, s);
+void turn(string& s, string from, string to) {
+    for (int i = 1; i + from.size() <= s.size() - 1; ++i) {
+        if (s.compare(i, from.size(), from) == 0) {
+            bool left = s[i - 1] == ' ' || ispunct(s[i - 1]);
+            bool right = s[i + from.size()] == ' ' || ispunct(s[i + from.size()]);
+            if (left && right) {
+                s.replace(i, from.size(), to);
+                i += to.size() - 1;
+            }
+        }
+    }
+}
+int main() {
+    string s, t;
+    getline(cin, t);
+    while (getline(cin, s)) {
         cout << s << "\nAI: ";
         s = " " + s + " ";
-        for (auto& i : s)
-        {
-            i |= (isupper(i) && i != 'I') * ' ';
-            i = (i == '?') ? '!' : i;
-        }
-        for (int i = 0; i < s.size() - 1; ++i)
-        {
+        for (int i = 0; i < s.size() - 1; ++i) {
+            s[i] |= (isupper(s[i]) && s[i] != 'I') * ' ';
+            s[i] = (s[i] == '?') ? '!' : s[i];
             if (s[i] == ' ' && (s[i + 1] == ' ' || ispunct(s[i + 1])))
                 s.erase(i--, 1);
         }
-        auto turn = [&](string from, string to) {
-            for (int i = 1; i + from.size() <= s.size() - 1;)
-            {
-                if (s.compare(i, from.size(), from) == 0)
-                {
-                    int r = i + from.size();
-                    bool left = s[i - 1] == ' ' || ispunct(s[i - 1]);
-                    bool right = s[r] == ' ' || ispunct(s[r]);
-                    if (left && right)
-                    {
-                        s.replace(i, from.size(), to);
-                        i += to.size();
-                        continue;
-                    }
-                }
-                ++i;
-            }
-        };
-        turn("I", "\n\n");
-        turn("me", "\n\n");
-        turn("can you", "I can");
-        turn("could you", "I could");
-        turn("\n\n", "you");
-        s = s.substr(1, s.size() - 2);
+        turn(s, "I", "\n\n");
+        turn(s, "me", "\n\n");
+        turn(s, "can you", "I can");
+        turn(s, "could you", "I could");
+        turn(s, "\n\n", "you");
+        auto f = [](char c) { return c != ' '; };
+        s.erase(s.begin(), find_if(s.begin(), s.end(), f));
+        s.erase(find_if(s.rbegin(), s.rend(), f).base(), s.end());
         cout << s << '\n';
     }
 }
